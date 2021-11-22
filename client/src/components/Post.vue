9de00fc26082d4b113f5e394671c941fddb9c292
CKubisa <ckubisa38@gmail.com>
2021-11-21 22:42:28 -0500

    Activty Page Working Correct

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -3,7 +3,6 @@
   <div class="card-image">
     <figure class="image is-4by3">
       <img :src="post.src" :alt="post.alt">
-        <button class="delete" @click="$emit('remove')"></button>
     </figure>
   </div>
   <div class="card-content">
@@ -15,23 +14,17 @@
       </div>
       <div class="media-content">
         <p class="title is-4"> {{post.user.firstName}} {{post.user.lastName}} </p>
-        <p class="subtitle is-6">{{post.user.handle}}</p>
+        <p class="subtitle is-6">
+            {{post.user.handle}}
+                  <time :datetime="post.time">{{prettyDate}}</time>
+        </p>
       </div>
     </div>
 
     <div class="content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-      <a href="#">#css</a> <a href="#">#responsive</a>
-      <br>
-      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+      {{post.caption}}
     </div>
   </div>
-    <footer class="card-footer">
-        <a href="#" class="card-footer-item">Save</a>
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-  </footer>
 </div>
 </template>
 
@@ -39,14 +32,19 @@
 export default {
     props: {
         post: Object
+    },
+    computed: {
+        prettyDate(){
+            if(this.post.time && this.post.time.toDateString){
+                return this.post.time.toDateString()
+            }else{
+                return 'Never'
+            }
+            
+        }
     }
 }
 </script>
 
 <style>
-    button.delete {
-        position: absolute;
-        top: 5px;
-        right: 5px;
-    }
 </style>
