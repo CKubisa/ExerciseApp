f2c4e0739b1dee8dd171e2feb7c1dc207425447a
CKubisa <ckubisa38@gmail.com>
2021-12-16 13:03:17 -0500

    Autofill working without server

--- client/src/views/FindFriends.vue
+++ client/src/views/FindFriends.vue
@@ -1,51 +1,34 @@
 <template>
-    <div class="about">
-        <h1 class="title">Find Friends</h1>
-
-        <table class="table is-striped is-hoverable is-fullwidth">
-            <thead>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Handle</th>
-                    <th>Profile Pic</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <tr v-for="u in list" :key="u.handle">
-                    <th>{{ u.firstName }}</th>
-                    <th>{{ u.lastName }}</th>
-                    <td>{{ u.handle }}</td>
-                    <td>
-                        <img :src="u.pic" width="50" :alt="u.handle" />
-                    </td>
-                    <td>
-                        <p class="buttons">
-                            <button class="button">
-                                <span class="icon is-small">
-                                    <i class="fas fa-eye"></i>
-                                </span>
-                            </button>
-                        </p>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+  <section>
+    <p class="content"><b>Selected:</b> {{ selected }}</p>
+    <o-field label="Find a JS framework">
+      <o-autocomplete rounded expanded v-model="name" :data="filteredDataArray" placeholder="e.g. jQuery" icon="search" clearable @select="option => selected = option">
+        <template v-slot:empty>No results found</template>
+      </o-autocomplete>
+    </o-field>
+  </section>
 </template>
 
-<script>import {  GetAll } from "../services/users"
-export default {
+<script>
+  export default {
     data() {
-        return {
-            list: []
-        }
+      return {
+        data: ['Angular', 'Angular 2', 'Aurelia', 'Backbone', 'Ember', 'jQuery', 'Meteor', 'Node.js', 'Polymer', 'React', 'RxJS', 'Vue.js'],
+        name: '',
+        selected: null
+      }
     },
-    async mounted(){
-        this.list = await GetAll();
+    computed: {
+      filteredDataArray() {
+        return this.data.filter(option => {
+          return (
+            option
+              .toString()
+              .toLowerCase()
+              .indexOf(this.name.toLowerCase()) >= 0
+          )
+        })
+      }
     }
-}
+  }
 </script>
